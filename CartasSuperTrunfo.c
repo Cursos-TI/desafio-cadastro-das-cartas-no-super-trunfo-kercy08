6c56a86015188c2696ecfaf3d5a839c49214bdc0
kercy08 <halankercy@hotmail.com>
2025-01-24 15:27:54 +0000

    criei printf e scanf 2x

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,10 +2,17 @@
 
     int main() {
     printf("Desafio do Xadrez!\n");
-        int população, pontos turísticos;
+        int população, pontos;
         float área;
         double pib;
         char estado, cidade [20];
+        
+        printf("Digite a população do estado: \n");
+        scanf("%d", &população);
+
+        printf("Pontos Turísticos: \n");
+        scanf("%d", &pontos);
+
         
    
 
